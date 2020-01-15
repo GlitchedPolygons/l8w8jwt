8a16b555c5f25a47e0772e7d72e48773fde85712
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-01-15 17:48:15 +0100

    Initial commit.

---
+++ library.c
@@ -0,0 +1,5 @@
+#include "library.h"
+
+#include <stdio.h>
+
+void hello(void) { printf("Hello, World!\n"); }
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef L8W8JWT_LIBRARY_H
+#define L8W8JWT_LIBRARY_H
+
+void hello(void);
+
+#endif // L8W8JWT_LIBRARY_H
