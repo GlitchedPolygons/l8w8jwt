7846f1950fceb4b0e47d25f153b36ad9f25ff73f
Glitched Polygons <37942667+GlitchedPolygons@users.noreply.github.com>
2020-02-02 18:50:00 +0100

    Update decode.c

--- src/decode.c
+++ src/decode.c
@@ -89,7 +89,7 @@ static int l8w8jwt_parse_claims(chillbuff* buffer, char* json, const size_t json
         while (*(end - 1) == '\\')
         {
             end = strchr(end + 1, '\"');
-            if (end == NULL)
+            if (end == NULL || end - json >= json_length)
             {
                 return L8W8JWT_CLAIM_PARSE_FAILURE;
             }
