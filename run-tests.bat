d2c229d0643c0de1fc78bcfbf5a4b675e2150bac
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-02-10 23:43:16 +0100

    Modified CMake setup (now using options inside root CMakeLists.txt for enabling/disabling tests, examples, etc...).

--- run-tests.bat
+++ run-tests.bat
@@ -1,9 +1,9 @@
 SET repo=%~dp0
-SET out="%repo%\tests\build"
+SET out="%repo%\build-msvc"
 if exist %out% ( rd /s /q %out% ) 
 mkdir %out% && cd %out%
-cmake -DBUILD_SHARED_LIBS=Off -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DBUILD_SHARED_LIBS=Off -DL8W8JWT_ENABLE_TESTS=On -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
 call Debug\run_tests.exe
-cd ..\..
+cd ..
 
--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 REPO=$(dirname "$0")
-rm -rf $REPO/tests/build/
-cd $REPO/tests/ && mkdir build && cd build
-cmake -DBUILD_SHARED_LIBS=Off .. && make
+rm -rf $REPO/build/
+mkdir build && cd build
+cmake -DBUILD_SHARED_LIBS=Off -DL8W8JWT_ENABLE_TESTS=On .. && make
 ./run_tests
