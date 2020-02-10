86f47594f0afba38fd97d72f320f6067401c92e7
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-02-10 15:12:03 +0100

    modified:   run-tests.bat

--- run-tests.bat
+++ run-tests.bat
@@ -1,6 +1,7 @@
-cd "tests"
-if exist "build" ( rd /s /q "build") 
-mkdir "build" && cd "build"
+SET repo=%~dp0
+SET out="%repo%\tests\build"
+if exist %out% ( rd /s /q %out% ) 
+mkdir %out% && cd %out%
 cmake -DBUILD_SHARED_LIBS=Off -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
 call Debug\run_tests.exe
