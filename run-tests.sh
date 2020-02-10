8a6fd47b6f46d639e90d8f95f8ff5b84a68f487f
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-02-11 00:27:33 +0100

    Added Codecov command to CircleCI config.

--- run-tests.sh
+++ run-tests.sh
@@ -2,5 +2,6 @@
 REPO=$(dirname "$0")
 rm -rf $REPO/build/
 mkdir build && cd build
-cmake -DBUILD_SHARED_LIBS=Off -DL8W8JWT_ENABLE_TESTS=On .. && make
+cmake -DBUILD_SHARED_LIBS=Off -DL8W8JWT_ENABLE_TESTS=On -DENABLE_COVERAGE=On .. && make
 ./run_tests
+cd $REPO
