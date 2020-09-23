ff7b91df4f063c8fc42184fcedbd6d838cd621f6
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-09-23 10:21:07 +0200

    Removed test.bat (on Windows, just use the "Git Bash for Windows" CLI and run bash test.sh).

--- test.bat
+++
@@ -1,9 +0,0 @@
-SET repo=%~dp0
-SET out="%repo%\build-msvc"
-if exist %out% ( rd /s /q %out% ) 
-mkdir %out% && cd %out%
-cmake -DBUILD_SHARED_LIBS=Off -DL8W8JWT_ENABLE_TESTS=On -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .
-call Debug\run_tests.exe
-cd ..
-
