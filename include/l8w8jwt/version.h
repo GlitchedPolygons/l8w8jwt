fe81adbcb07658673ab49e9756285d959e0d1010
Glitched Polygons <glitchedpolygons@protonmail.com>
2023-09-04 22:25:45 +0200

    Upgraded underlying MbedTLS version to v3.4.1 (associated with #35). Bumped l8w8jwt's version number to 2.2.0

--- include/l8w8jwt/version.h
+++ include/l8w8jwt/version.h
@@ -30,12 +30,12 @@ extern "C" {
 /**
  * Current l8w8jwt version number.
  */
-#define L8W8JWT_VERSION 218
+#define L8W8JWT_VERSION 220
 
 /**
  * Current l8w8jwt version number (as a human-readable string).
  */
-#define L8W8JWT_VERSION_STR "2.1.8"
+#define L8W8JWT_VERSION_STR "2.2.0"
 
 #if defined(_WIN32) && defined(L8W8JWT_DLL)
 #ifdef L8W8JWT_BUILD_DLL
