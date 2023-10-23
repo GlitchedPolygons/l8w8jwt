824bb52799a8ea55a12c75af9280ed9c6f73ebf4
Glitched Polygons <glitchedpolygons@protonmail.com>
2023-10-23 11:10:28 +0200

    Increased version number to 2.2.1

--- include/l8w8jwt/version.h
+++ include/l8w8jwt/version.h
@@ -30,12 +30,12 @@ extern "C" {
 /**
  * Current l8w8jwt version number.
  */
-#define L8W8JWT_VERSION 220
+#define L8W8JWT_VERSION 221
 
 /**
  * Current l8w8jwt version number (as a human-readable string).
  */
-#define L8W8JWT_VERSION_STR "2.2.0"
+#define L8W8JWT_VERSION_STR "2.2.1"
 
 #if defined(_WIN32) && defined(L8W8JWT_DLL)
 #ifdef L8W8JWT_BUILD_DLL
